b1e60d0d89bc614640ced46384e72638f6c3ef73
Thomas Gredig <thomas.gredig@csulb.edu>
2019-12-08 11:20:43 -0800

    add Arduino sketch

---
+++ arduino-sketch-read-analog.ino
@@ -0,0 +1,24 @@
+/*
+  Measure Analog Pin 0
+  by Thomas Gredig
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1);              // wait for a second
+  long a = analogRead(A0);
+  if(a>=0) {
+    Serial.println(a);
+  }
+}
--- read.Arduino.R
+++ read.Arduino.R
@@ -2,22 +2,31 @@
 library(serial)
 
 # Start program in Arduino and find port
-con <- serialConnection(name = "testcon",port = "cu.usbserial-DA00VRGH"
-                        ,mode = "9600,n,8,1"
-                        ,newline = 1
-                        ,translation = "crlf"
+# in this case the result from A0 is sent
+con <- serialConnection(name = "test",
+                        port = "cu.usbserial-DA00VRGH",
+                        mode = "9600,n,8,1",
+                        newline = 1, 
+                        buffering = "none",
+                        translation = "auto"
 )
 
 c=0
 # open connection and output data
 open(con)
+r= c()
 while(c<100) {
   a = read.serialConnection(con)
   if(!is.null(a)) {
-    print(paste(a," - "))
-    c = c+1;
+    print(paste0("[",a,"]"))
+    a1 = as.numeric(a)
+    if (a1>=0 & a1<=1023) {
+      r = c(r,a1)
+      c = c+1;
+    }
   }
 }
 
 close(con)
 
+plot(r)
